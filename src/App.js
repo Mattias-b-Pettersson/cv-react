be952a3bf00991848211e2485593d386c9369d7e
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-01-29 14:46:54 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Routes>
+        <Route path="/" element={<About />} />
+      </Routes>
     </div>
   );
 }
