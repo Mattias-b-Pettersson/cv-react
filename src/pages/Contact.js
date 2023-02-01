a48ac0ca6500929618a9ebde8fb3ce91c2333e3b
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-02-01 20:28:59 +0100

    Add animation to form and change "sent" view

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
-import { Container, Form, Button, Col, Row } from 'react-bootstrap'
+import { Container, Form, Col, Row, Card, Image } from 'react-bootstrap'
 import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPhoneAlt, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
+import styles from '../styles/Contact.module.css'
+import envelope from '../assets/images/envelope.png'
 
 function Contact() {
     const [buttonState, setButtonState] = useState("Skicka")
+    const [sentMail, setSentMail] = useState(false)
     const [formInfo, setFormInfo] = useState({
         email: '',
         number: '',
@@ -51,7 +55,10 @@ function Contact() {
         axios.post('https://api.emailjs.com/api/v1.0/email/send', data, customConfig)
             .then(res => {
                 console.log(res)
-                setButtonState(<>Skickat! <FontAwesomeIcon icon={faThumbsUp} className="me-2"/></>)
+                setButtonState("Skickat!")
+                setInterval(() => {
+                    setSentMail(true)
+                }, 2000)
             })
             .catch(err => {
                 console.log(err)
@@ -64,39 +71,79 @@ function Contact() {
     
 
     return (
-        <Container className='border-top'>
-            <Row className='mt-5'>
-                <Col xs={12} lg={6} fluid="true" className='mx-auto'>
-                    <Form className='mb-5' onSubmit={onSubmit}>
-                        <h2><FontAwesomeIcon icon={faEnvelope} className="me-2"/>Kontakta mig</h2>
-                        <Form.Group className="mb-3" controlId="name">
-                            <Form.Label>Namn</Form.Label>
-                            <Form.Control type="text" name='name' placeholder="Namn" required={true} onChange={onChange} />
-                        </Form.Group>
+        sentMail ? (
+            <>
+                <div>
+                    <Container className='border-top'>
+                        <Row className='mt-5'>
+                            <Col xs={12} lg={6} fluid="true" className='mx-auto'>
+                                <h2><FontAwesomeIcon icon={faPhoneAlt} className="me-2"/>Kontaktuppgifter</h2>
+                                <p>Telefon: <a href="tel:070-6140570">070-614 05 70</a></p>
+                                <p>Email: <a href="mailto:Mattep3@gmail.com">Mattep3@gmail.com</a></p>
+                                <div >
+                                    <h1>Tack för ditt mail!</h1>
+                                    <p className='mb-3'>Jag svarar så fort jag kan.</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <div className={`${styles.thanksOuter} overflow-hidden w-100 mb-5`}>
+                        <motion.img
+                            src={envelope}
+                            alt=""
+                            className={styles.envelope}
+                            animate={{transition: { duration: 2 }, x:[200, 100, 5000], rotate: ["3deg", "-5deg", "-20deg"] }}
+                        />
+                    </div>
+                </div>
+            </>
+        ) : (
+            <Container className='border-top'>
+                <Row className='mt-5'>
+                    <Col xs={12} lg={6} fluid="true" className='mx-auto'>
+                        <h2><FontAwesomeIcon icon={faPhoneAlt} className="me-2"/>Kontaktuppgifter</h2>
+                        <p>Telefon: <a href="tel:070-6140570">070-614 05 70</a></p>
+                        <p>Email: <a href="mailto:Mattep3@gmail.com">Mattep3@gmail.com</a></p>
+                        <Form className='mb-5' onSubmit={onSubmit}>
+                            <h2><FontAwesomeIcon icon={faEnvelope} className="me-2"/>Kontakta mig</h2>
+                            <Form.Group className="mb-3" controlId="name">
+                                <Form.Label>Namn</Form.Label>
+                                <Form.Control type="text" name='name' placeholder="Namn" required={true} onChange={onChange} />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="email">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" name='email' placeholder="Email" required={true} onChange={onChange} />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" name='email' placeholder="Email" required={true} onChange={onChange} />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="number">
-                            <Form.Label>Telefonnummer</Form.Label>
-                            <Form.Control type="text" placeholder="Telefonnummer" onChange={onChange} name="number" />
-                            <Form.Text className='text-muted'>Du måste inte ange telefonnumer</Form.Text>
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="number">
+                                <Form.Label>Telefonnummer</Form.Label>
+                                <Form.Control type="text" placeholder="Telefonnummer" onChange={onChange} name="number" />
+                                <Form.Text className='text-muted'>Du måste inte ange telefonnumer</Form.Text>
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="message">
-                            <Form.Label>Meddelande</Form.Label>
-                            <Form.Control as="textarea" name="message" rows={6} placeholder="Skriv ditt meddelande här..." required={true} onChange={onChange}/>
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="message">
+                                <Form.Label>Meddelande</Form.Label>
+                                <Form.Control as="textarea" name="message" rows={6} placeholder="Skriv ditt meddelande här..." required={true} onChange={onChange}/>
+                            </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            {buttonState}
-                        </Button>
-                    </Form >
-                </Col>
-            </Row>
-        </Container>
+                            <motion.button 
+                            className={`${styles.btnColor} btn`} 
+                            type="submit"
+                            whileHover={{scale: 1.1, rotate: "-5deg"}} 
+                            whileTap={{ 
+                                scale: 0.9, 
+                                rotate: "5deg",
+                                transition: { duration: 0.2 }
+                            }}
+                            >
+                                {buttonState}
+                            </motion.button>
+                        </Form >
+                    </Col>
+                </Row>
+            </Container>
+        )
     )
 }
 
